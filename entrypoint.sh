ed218c602e1ace0e767199f036458438976de8bf
Sebastian Sch <sebassch@gmail.com>
2018-06-03 18:48:38 +0300

    Change chain names

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,12 +63,12 @@ LoadIptables()
     ip -f inet route add local default dev lo table 108
 
     # echo Clean Iptables
-    iptables -t mangle -F ISTIO_INBOUND 2>/dev/null
-    iptables -t mangle -X ISTIO_INBOUND 2>/dev/null
-    iptables -t mangle -F ISTIO_DIVERT 2>/dev/null
-    iptables -t mangle -X ISTIO_DIVERT 2>/dev/null
-    iptables -t mangle -F ISTIO_TPROXY 2>/dev/null
-    iptables -t mangle -X ISTIO_TPROXY 2>/dev/null
+    iptables -t mangle -F KUBEVIRT_INBOUND 2>/dev/null
+    iptables -t mangle -X KUBEVIRT_INBOUND 2>/dev/null
+    iptables -t mangle -F KUBEVIRT_DIVERT 2>/dev/null
+    iptables -t mangle -X KUBEVIRT_DIVERT 2>/dev/null
+    iptables -t mangle -F KUBEVIRT_TPROXY 2>/dev/null
+    iptables -t mangle -X KUBEVIRT_TPROXY 2>/dev/null
     iptables -t mangle -F PREROUTING 2>/dev/null
     iptables -t mangle -X PREROUTING 2>/dev/null
     iptables -t mangle -F OUTPUT 2>/dev/null
@@ -77,28 +77,29 @@ LoadIptables()
     echo "Iptables for tproxy"
 
     iptables -t mangle -vL
-    iptables -t mangle -N ISTIO_DIVERT
-    iptables -t mangle -A ISTIO_DIVERT -j MARK --set-mark 8
-    iptables -t mangle -A ISTIO_DIVERT -j ACCEPT
+    iptables -t mangle -N KUBEVIRT_DIVERT
+    iptables -t mangle -A KUBEVIRT_DIVERT -j MARK --set-mark 8
+    iptables -t mangle -A KUBEVIRT_DIVERT -j ACCEPT
 
     table=mangle
-    iptables -t ${table} -N ISTIO_INBOUND
+    iptables -t ${table} -N KUBEVIRT_INBOUND
     iptables -t ${table} -A PREROUTING -p tcp -m comment --comment "Kubevirt Spice"  --dport 5900 -j RETURN
     iptables -t ${table} -A PREROUTING -p tcp -m comment --comment "Kubevirt virt-manager"  --dport 16509 -j RETURN
-    iptables -t ${table} -A PREROUTING -p tcp -i vnet0 -j ISTIO_INBOUND
+    iptables -t ${table} -A PREROUTING -p tcp -i vnet0 -j KUBEVIRT_INBOUND
 
-    iptables -t ${table} -N ISTIO_TPROXY
-    iptables -t ${table} -A ISTIO_TPROXY ! -d 127.0.0.1/32 -p tcp -j TPROXY --tproxy-mark 8/0xffffffff --on-port 9401
-    #iptables -t mangle -A ISTIO_TPROXY ! -d 127.0.0.1/32 -p udp -j TPROXY --tproxy-mark 8/0xffffffff --on-port 8080
+    iptables -t ${table} -N KUBEVIRT_TPROXY
+    iptables -t ${table} -A KUBEVIRT_TPROXY ! -d 127.0.0.1/32 -p tcp -j TPROXY --tproxy-mark 8/0xffffffff --on-port 9401
+    #iptables -t mangle -A KUBEVIRT_TPROXY ! -d 127.0.0.1/32 -p udp -j TPROXY --tproxy-mark 8/0xffffffff --on-port 8080
 
     # If an inbound packet belongs to an established socket, route it to the
     # loopback interface.
-    iptables -t ${table} -A ISTIO_INBOUND -p tcp -m socket -j ISTIO_DIVERT
-    #iptables -t mangle -A ISTIO_INBOUND -p udp -m socket -j ISTIO_DIVERT
+    iptables -t ${table} -A KUBEVIRT_INBOUND -p tcp -m socket -j KUBEVIRT_DIVERT
+    #iptables -t mangle -A KUBEVIRT_INBOUND -p udp -m socket -j KUBEVIRT_DIVERT
 
     # Otherwise, it's a new connection. Redirect it using TPROXY.
-    iptables -t ${table} -A ISTIO_INBOUND -p tcp -j ISTIO_TPROXY
-    #iptables -t mangle -A ISTIO_INBOUND -p udp -j ISTIO_TPROXY
+    iptables -t ${table} -A KUBEVIRT_INBOUND -p tcp -j KUBEVIRT_TPROXY
+    #iptables -t mangle -A KUBEVIRT_INBOUND -p udp -j KUBEVIRT_TPROXY
+    iptables -t ${table} -I OUTPUT 1 -d 10.0.1.2 -j ACCEPT
 
     table=nat
     # Remove vm Connection from iptables rules
@@ -107,9 +108,6 @@ LoadIptables()
 
     # Allow guest -> world -- using nat for UDP
     iptables -t ${table} -I POSTROUTING 1 -s 10.0.1.2 -p udp -j MASQUERADE
-
-    table=mangle
-    iptables -t ${table} -I OUTPUT 1 -d 10.0.1.2 -j ACCEPT
 
     return 0
 }
